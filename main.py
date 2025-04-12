2960d880419b050ef7eb0c6c2540531b0a5de325
Admin <ulanapolancikova@gmail.com>
2025-04-12 14:04:05 +0300

    captain

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from data.users import User
+from data.jobs import Jobs
+from data import db_session
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/mars_explorer.sqlite")
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
+
+user = User()
+user.surname = "Scott"
+user.name = "Ridley"
+user.age = 21
+user.position = "captain"
+user.speciality = "research engineer"
+user.address = "module_1"
+user.email = "scott_chief@mars.org"
+
+db_sess = db_session.create_session()
+db_sess.add(user)
+db_sess.commit()
+
+user = User()
+user.surname = "Waters"
+user.name = "Lowell"
+user.age = 23
+user.position = "engineer"
+user.speciality = "mechanical"
+user.address = "module_2"
+user.email = "waters_mechanical@mars.org"
+
+db_sess = db_session.create_session()
+db_sess.add(user)
+db_sess.commit()
+
+user = User()
+user.surname = "Armstrong"
+user.name = "Sylvia"
+user.age = 25
+user.position = "pilot"
+user.speciality = "spacecraft pilot"
+user.address = "module_3"
+user.email = "sylvia_pilot@mars.org"
+
+db_sess = db_session.create_session()
+db_sess.add(user)
+db_sess.commit()
+
+user = User()
+user.surname = "Johnson"
+user.name = "Emily"
+user.age = 30
+user.position = "scientist"
+user.speciality = "astronomy scientist"
+user.address = "module_4"
+user.email = "johnson_scientist@mars.org"
+
+db_sess = db_session.create_session()
+db_sess.add(user)
+db_sess.commit()
